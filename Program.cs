1a615819e57cf9a521918a8f3b4cb2992a89b79f
Vyvy-vi <vyom.j@protonmail.com>
2021-01-11 09:58:25 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MLH LOCALHACKDAY: BUILD!");
+            var name="Vyvy-vi";
+            console.WriteLine($"I am {name}");
+        }
+    }
+}
